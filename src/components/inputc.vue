bf1dbaa9530c254cd8bfbf50b0f0bffd083bd73f
pentaking <lasking3@163.com>
2020-06-16 17:42:20 +0800

    v3.0

--- src/components/inputc.vue
+++ src/components/inputc.vue
@@ -1,55 +1,70 @@
 <template>
-<div>
-<div class="ipt" :class="{'is-err':error}">
-    <!-- 输入框 类型 值 提示 名字 方法-->
-    <input  :type="type"  :value="value" :placeholder="placeholder" :name="name" @input="$emit('input',$event.target.value)" >
-    <!-- 内按钮 隐藏 变灰 事件-->
-    <button v-if="btnt" :disabled="disabled"  @click="$emit('btnclick')" >{{btnt}}</button>
-    
-    
-</div>
-<!-- 提示框 -->
-<div v-if="error" class="ierr">{{error}}</div>
-</div>
+  <div>
+    <div
+      class="ipt"
+      :class="{'is-err':error}"
+    >
+      <!-- 输入框 类型 值 提示 名字 方法-->
+      <input
+        :type="type"
+        :value="value"
+        :placeholder="placeholder"
+        :name="name"
+        @input="$emit('input',$event.target.value)"
+      >
+      <!-- 内按钮 隐藏 变灰 事件-->
+      <button
+        v-if="btnt"
+        :disabled="disabled"
+        @click="$emit('btnclick')"
+      >{{btnt}}</button>
+
+    </div>
+    <!-- 错误提示框 -->
+    <div
+      v-if="error"
+      class="ierr"
+    >{{error}}</div>
+  </div>
 </template>
 
 <script>
-export default 
-{
-    props:{
-        type:String,
-        value:String,
-        placeholder:String,
-        name:String,
-        btnt:String,
-        disabled:Boolean,
-        error:{}
-    }
- }
+export default {
+  props: {
+    type: String,
+    value: String,
+    placeholder: String,
+    name: String,
+    btnt: String,
+    disabled: Boolean,
+    error: {}
+  }
+};
 </script>
 
  <style lang="less" scoped>
- .ipt{
-     display: flex;
-     width: 300px;
-     height: 30px;
-     border: 1px solid gray;
+.ipt {
+  display: flex;
+  width: 300px;
+  height: 30px;
+  border: 1px solid gray;
 
-     input{
-         border: none;
-         width: 100%;
-         font-size: 16px;
-     }
-     button{
-         width: 110px;
-         background-color: royalblue;
-         border: none;
- }}
+  input {
+    border: none;
+    width: 100%;
+    font-size: 16px;
+  }
+  button {
+    width: 110px;
+    background-color: royalblue;
+    border: none;
+  }
+}
 
- .ierr{
-     color: red;
- }
- .is-err{
-     border-color: red;
- }
- </style>
+.ierr {
+  color: red;
+}
+.is-err {
+  border-color: red;
+}
+</style>
--- src/views/Loginc.vue
+++ src/views/Loginc.vue
@@ -1,121 +1,133 @@
 <template>
   <div class="loginc">
-    <div><img src="@/assets/logo.png" alt=""></div>
+    <div><img
+        src="@/assets/logo.png"
+        alt=""
+      ></div>
     <div class="lp">
       <!-- 号码输入框 -->
-    <inputc type="tel" v-model="phone" placeholder="手机号"  :btnt="btnt" :disabled="disabled" :error="error.phone" @btnclick="btnclick1"/>
-     <!-- 验证码输入框 -->
-    <inputc type="text" v-model="code" placeholder="验证码" :error="error.code" />
-     <!-- 登陆框 -->
-   
+      <inputc
+        type="tel"
+        v-model="phone"
+        placeholder="手机号"
+        :btnt="btnt"
+        :disabled="disabled"
+        :error="error.phone"
+        @btnclick="btnclick1"
+      />
+      <!-- 验证码输入框 -->
+      <inputc
+        type="text"
+        v-model="code"
+        placeholder="验证码"
+        :error="error.code"
+      />
+      <!-- 登陆框 -->
+
     </div>
     <!-- 登陆按钮 -->
-    <div><button @click="login">登陆</button></div> 
-    
+    <div><button
+        :disabled="disabledl"
+        @click="login"
+      >{{logint}}</button></div>
 
   </div>
 </template>
 
 <script>
-import inputc from '@/components/inputc.vue';
+import inputc from "@/components/inputc.vue";
 export default {
-
-  data(){
-    return{
-      code:"",
-      phone:"",
-      btnt:"获取验证码",
-      error:{},
-      disabled:false
-    }
+  data() {
+    return {
+      code: "",
+      phone: "",
+      btnt: "获取验证码",
+      error: {},
+      disabled: false,
+      disabledl: false,
+      logint: "登陆"
+    };
   },
 
   components: {
     inputc
-  }
-,
-  methods:{
+  },
+  methods: {
     // 输入框
-    input(){
+    input() {},
+    // 手机号验证
+    vphone() {
+      if (!/^1[34578]\d{9}$/.test(this.phone)) {
+        this.$set(this.error, "phone", "号码错误");
+        return false;
+      } else {
+        this.$set(this.error, "");
+        return true;
+      }
+    },
+    // 验证码6位数字
+    changecode() {
+      if (!/^\d{6}$/.test(this.code)) {
+        this.$set(this.error, "code", "验证码错误");
+        return false;
+      } else {
+        this.$set(this.error, "");
+        return true;
+      }
+    },
 
-    },
-    // 手机号
-    vphone(){
-      if (!(/^1[34578]\d{9}$/.test(this.phone))){
-        this.error.phone="号码错误"
-        return false
-      }
-      else  {
-        this.error={}
-        return true
-      }
-      
-    },
-    // 验证码
-    changecode(){
-      if (!(/^\d{6}$/.test(this.phone))){
-        this.error.code="验证码错误"
-        return false
-      }
-      else  {
-        this.error={}
-        return true
-      }
-      
-    },
-    
-    btnclick1(){
-      
-      if (this.vphone()){
+    btnclick1() {
+      if (this.vphone()) {
         // 发送验证码请求
 
-
         // 验证码倒计时
-      
-      let time=59
-      let timer=setInterval(()=>{
-        if (time==0){
-          clearInterval(timer)
-          this.btnt="获取验证码"
-          this.disabled= false
 
-        }
-        else{
-          this.btnt=time+"秒重试"
-          this.disabled=true
-          time--
-          
-        }
-      },1000)
+        let time = 59;
+        let timer = setInterval(() => {
+          if (time == 0) {
+            clearInterval(timer);
 
+            this.btnt = "获取验证码";
+            this.disabled = false;
+          } else {
+            this.btnt = time + "秒重试";
+            this.disabled = true;
+            time--;
+          }
+        }, 1000);
       }
-      
-
     },
     // 登陆
-    login(){
-if (this.changecode()){
-  
-  
-  console.log(this.error);
-  console.log(this.phone+"+"+this.code);
-  
-  
-  // 发送登陆请求
-}
+    login() {
+      if (this.changecode()) {
+        // 发送登陆请求
+        console.log(this.phone + "+" + this.code);
+        // 登陆点击拦截3秒
+        let time = 3;
+        let timer = setInterval(() => {
+          if (time == 0) {
+            clearInterval(timer);
+            this.logint = "登陆";
+            this.disabledl = false;
+          } else {
+            this.logint = time + "秒重试";
+            this.disabledl = true;
+            time--;
+          }
+        }, 1000);
+      }
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-
-.loginc{
-  display :flex;
+.loginc {
+  display: flex;
   flex-direction: column;
   align-items: center;
-  
-  button{
+
+  button {
     width: 300px;
     height: 30px;
     background-color: yellowgreen;
@@ -123,5 +135,4 @@ if (this.changecode()){
     font-size: 16px;
   }
 }
-
 </style>
